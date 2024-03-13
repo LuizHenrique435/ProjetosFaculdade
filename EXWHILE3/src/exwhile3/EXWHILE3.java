79cde5938c49471a89d94aeca159035597bd0731
Luiz Henrique Souza Silva <141936881+LuizHenrique435@users.noreply.github.com>
2024-03-13 12:31:36 -0300

    Exercicio Java

---
+++ EXWHILE3/src/exwhile3/EXWHILE3.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exwhile3;
+
+/**
+ *
+ * @author aluno
+ */
+public class EXWHILE3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int i = 0;
+        
+        while(i<=30){
+            i++;
+            if(i%3==0){
+                System.out.println(i);
+            }
+        }
+        
+    }
+    
+}
